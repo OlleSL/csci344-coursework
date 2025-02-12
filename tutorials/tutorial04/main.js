bb98c440e7589d92d3a48f915388b551324c5c18
Olle <osonesso@unca.edu>
2025-02-12 16:04:47 -0500

    Quiz 1 Done!

--- tutorials/tutorial04/main.js
+++ tutorials/tutorial04/main.js
@@ -9,7 +9,8 @@ function setup() {
   // invoke any drawing functions inside of setup.
   // functions should all go between "createCanvas()" and "drawGrid()"
   draw5Circles();
-  // draw5RedSquares();
+  draw5CirclesFor();
+  draw5RedSquares();
   drawGrid(canvasWidth, canvasHeight);
 }
 
@@ -23,15 +24,29 @@ function draw5Circles() {
   let i = 0;
 
   while (i < 5) {
-    cirlce(x, y + 50 * i, d);
+    circle(x, y + 50 * i, d);
     ++i;
   }
 
-  //   circle(100, 200, 50); // centerX, centerY, radius
-  //   circle(100, 250, 50);
-  //   circle(100, 300, 50);
-  //   circle(100, 350, 50);
-  //   circle(100, 400, 50);
+  // circle(100, 200, 50); // centerX, centerY, radius
+  // circle(100, 250, 50);
+  // circle(100, 300, 50);
+  // circle(100, 350, 50);
+  // circle(100, 400, 50);
+}
+
+function draw5CirclesFor() {
+  noFill();
+  // fill('red');
+  let x = 200;
+  let y = 200;
+  let d = 50;
+  let i = 0;
+
+  for (let index = 0; index < 5; index++) {
+    circle(x, y + 50 * i, d);
+    ++i;
+  }
 }
 
 function draw5RedSquares() {
